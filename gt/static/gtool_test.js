3f01e4117472afdb847a2b140971ddcd340db609
ZhongPing <zhizhi100@foxmail.com>
2015-11-16 17:17:27 +0800

    static files

--- gt/static/gtool_test.js
+++ gt/static/gtool_test.js
@@ -1,3 +1,54 @@
-jq(document).ready(function(){
-	alert('JQuery')
-})
+window.test = function(msg){
+	//alert(msg)
+	jq("#nsrxxForm_nsrsbh").val("430111338481825");
+	var e = jq.Event("keyup");
+	e.keyCode = 13;
+	jq("#nsrxxForm_nsrsbh").trigger(e);
+};
+
+var gthelp = function () {
+	/*
+	swordAlertWrong("test", {
+		width : 770,
+		height : 500,
+		isMax : "false",
+		titleName : "系统异常提示"
+	});*/
+	var gydrctrl = ctrl + "_getDrxx";// 导入处理自定义的ctrl
+	var drxxBtn = new SwordSubmit();
+	//drxxBtn.pushData('gydrCtrl', gydrctrl);
+	//drxxBtn.setCtrl('GYDrCtrl_openDr');// 公用导入方法
+	drxxBtn.submit = function(){
+		alert('submit')
+	}
+	iframe = swordAlertIframe('helloword.html', {
+				titleName : "申报信息导入",
+				width : 600,
+				height : 480,
+				param : window,
+				isNormal : 'true',
+				isMax : 'true',
+				isClose : 'true',
+				isMin : "false",
+				submit : drxxBtn
+			});
+	// jq(iframe).find("input").val(999);
+	// jq(ifrmae).find("button").bind("click",function(){
+		// alert("999")
+	// })
+	console.log(iframe)
+};
+
+jq(document).ready(function () {
+	jq("#nsrxxForm_nsrsbh").each(function () {
+		var nsr = jq(this)
+			nsr.css("width", "80%");
+		nsr.parent().append("<a id='golabel' href='javascript:void(0)' onclick='gthelp()'>go</a>");
+		window.onhelp = new Function("return false;");
+		nsr.bind("keyup", function (e) {
+			if (e.keyCode == 112) {
+				gthelp();
+			}
+		});
+	});
+});
